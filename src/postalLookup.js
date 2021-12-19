c67ad542abb79d005818254513b0bbc689ce91f2
“TracyCrape” <tcrape@gmail.com>
2021-12-18 19:33:27 -0700

    creation of three pages and navigation bar

---
+++ src/postalLookup.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function PostalLookup() {
+  return (
+    <div>
+      <h1>Postal Code Lookup</h1>
+    </div>
+  );
+}
+
+export default PostalLookup;
---
+++ src/universities.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function Universities() {
+  return (
+    <div>
+      <h1>Universities</h1>
+    </div>
+  );
+}
+
+export default Universities;
